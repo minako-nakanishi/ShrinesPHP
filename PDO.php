4641021fedb53847a19d4b72ccae011ca43004d0
Minako Nakanishi <n135216e@gmail.com>
2019-03-08 11:08:18 +0900

    DB接続処理にエラーモードとえみゅれーしょんを記載。

--- PDO.php
+++ PDO.php
@@ -12,6 +12,8 @@ function db_connect(){
         //PDOの引数に渡す情報を変数へ格納
         $dsn = "$db_type:host=$db_host;dbname=$db_name;charset=utf8";
         $pdo = new PDO($dsn,$db_user,$db_pass); //DBへ接続
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //エラー発生時は例外処理を実施
+        $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
         print "接続に成功しました";
     
     } catch (PDOException $Exception){
