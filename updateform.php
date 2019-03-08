9191067cc793d1ca2fdd7449d4a91f15ea8c4212
Minako Nakanishi <n135216e@gmail.com>
2019-03-08 10:28:20 +0900

    登録画面及び更新画面のの項目作成

--- updateform.php
+++ updateform.php
@@ -1,2 +1,28 @@
-<?php
-
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>新規登録画面</title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <div style="font-size: 22px;">新規登録画面</div>
+        <hr>
+        <form name="insert" method="post" action="list.php">
+            <div>
+                <label>　神社名　：<input type="text" name="name" maxlength="50"></label>
+            </div>
+            <div>
+                <label>　ふりがな：<input type="text" name="kana" maxlength="100"></label>
+            </div>
+            <div>
+                <label>　都道府県：<input type="text" name="prefecture" maxlength="20"></label>
+            </div>
+            <div>
+                <label>　最寄駅　：<input type="text" name="station" maxlength="50"></label>
+            </div>
+            <div>
+                <input type="submit" value="更新" style="margin-left: 200px;">
+            </div>
+        </form>
+    </body>
+</html>
