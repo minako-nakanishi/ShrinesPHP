416eb0a7ba729997130e9cde9b18d6e18ea6977a
Minako Nakanishi <n135216e@gmail.com>
2019-03-08 14:29:17 +0900

    index.htmlをindex.phpへ変更（検索・一覧画面）

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
-<?php
-
-
-
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>メイン画面</title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <div style="font-size: 22px;">検索・一覧画面</div>
+        <hr>
+        <form name="main" method="post" action="index.php">
+            <label>名前：<input type="text" name="search" required></label>
+            <input type="submit" value="検索">
+        </form>
+    </body>
+</html>
