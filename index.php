f773080cdb9497b5571454378bdae93ec77f6aa1
Minako Nakanishi <n135216e@gmail.com>
2019-03-08 17:57:53 +0900

    検索機能の追加

--- index.php
+++ index.php
@@ -1,24 +1,35 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>メイン画面</title>
+        <title>検索・一覧画面</title>
         <meta charset="UTF-8">
     </head>
     <body>
         <span style="font-size: 22px;">検索・一覧画面</span>&nbsp;
         <span><a href="insert.html">新規登録画面</a></span>
         <hr>
+        <!--検索機能-->
         <form name="main" method="post" action="index.php">
-            <label>名前：<input type="text" name="search" required></label>
+            <label>名前：<input type="text" name="search"></label>
             <input type="submit" value="検索">
         </form>
         <?php
         require_once('PDO.php'); //DB接続用phpの読み込み
         $pdoM = db_connect();
         try {
-           $pdoM->beginTransaction();//トランザクション
-           $sql = "SELECT * FROM shrines";
-           $stml = $pdoM->prepare($sql); //sqlの解析
+           $pdoM->beginTransaction();//トランザクション開始
+           if(isset($_POST['search']) && $_POST['search'] != ""){
+               $search_key = '%'.$_POST['search'].'%';
+               $sql = "SELECT * FROM shrines WHERE name like :name OR prefecture like :prefecture OR station like :station";
+               $stml = $pdoM->prepare($sql); //sqlの解析
+               $stml->bindValue(':name', $search_key,PDO::PARAM_STR);
+               $stml->bindValue(':prefecture', $search_key,PDO::PARAM_STR);
+               $stml->bindValue(':station', $search_key,PDO::PARAM_STR);
+               
+           }else{
+               $sql = "SELECT * FROM shrines";
+               $stml = $pdoM->prepare($sql); //sqlの解析
+           }
            $stml->execute();//sqlの実行
         } catch (PDOException $Exception) {
             $pdoM->rollBack();
@@ -30,6 +41,8 @@
         }else{
         ?>
         <br><br>
+        
+        <!-- 初期表示の一覧画面(全件検索) -->
         <table border="1">
             <tbody>
                 <tr><th>神社名</th><th>都道府県</th><th>最寄駅</th></tr>
