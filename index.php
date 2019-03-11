4339f634412209409c29379010e1bf0b012a8d38
Minako Nakanishi <n135216e@gmail.com>
2019-03-11 09:40:38 +0900

    コメントの追記

--- index.php
+++ index.php
@@ -15,17 +15,19 @@
         </form>
         <?php
         require_once('PDO.php'); //DB接続用phpの読み込み
-        $pdoM = db_connect();
+        $pdoM = db_connect(); //DBへ接続するメソッドの呼び出し.
         try {
            $pdoM->beginTransaction();//トランザクション開始
+           // 検索用テキストボックスに値が入っている場合
            if(isset($_POST['search']) && $_POST['search'] != ""){
                $search_key = '%'.$_POST['search'].'%';
                $sql = "SELECT * FROM shrines WHERE name like :name OR prefecture like :prefecture OR station like :station";
                $stml = $pdoM->prepare($sql); //sqlの解析
+               /* 検索値をsqlにバインドする */
                $stml->bindValue(':name', $search_key,PDO::PARAM_STR);
                $stml->bindValue(':prefecture', $search_key,PDO::PARAM_STR);
                $stml->bindValue(':station', $search_key,PDO::PARAM_STR);
-               
+               /* 初期表示や検索値が空の場合 */
            }else{
                $sql = "SELECT * FROM shrines";
                $stml = $pdoM->prepare($sql); //sqlの解析
@@ -35,14 +37,15 @@
             $pdoM->rollBack();
             die('エラー：'.$Exception->getMessage());
         }
-        $count = $stml->rowCount(); //件数のカウント
+      
+        $count = $stml->rowCount(); //ヒットした件数のカウント
         if($count<1){
           print "登録されているデータはありません";  
         }else{
         ?>
         <br><br>
         
-        <!-- 初期表示の一覧画面(全件検索) -->
+        <!--一覧画面の表示-->
         <table border="1">
             <tbody>
                 <tr><th>神社名</th><th>都道府県</th><th>最寄駅</th></tr>
